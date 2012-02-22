ba2ccf2b07471f81a675ed69a1739e61fbe9c21a
Garen Torikian <gjtorikian@gmail.com>
2012-02-21 19:05:23 -0800

    Mostly a bunch of bugfixes--now ready for production use

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ var Manifest = require('./lib/manifest'),
     Generator = require('./lib/generator');
 
 var conrefs = require('markdown_conrefs'),
+    jade = require('jade'),
     async = require('async');
 
-exports.Manifest = Manifest;
+//exports.Manifest = Manifest;
 exports.Generator = Generator;
 
 exports.open = function(filename, callback) {
-  return Manifest.load(filename, callback);
+  Manifest.open(filename, callback);
 };
 
-exports.makeConrefs = function() {
+exports.makeConrefs = function(cb) {
   conrefs.init(Manifest.options.files);
+  cb(null);
 };
 
 exports.createGenerator = function(options, callback) {
-  callback(null, Generator.create(options));
+  Generator.createGenerator(options, callback);
 };
 
-exports.copyAssets = function(srcDir, destDir) {
+exports.copyAssets = function(srcDir, destDir, callback) {
   console.log("Copying assets...");
-  Generator.copyAssets(srcDir, destDir);
+  Generator.copyAssets(srcDir, destDir, callback);
 };
 
-exports.generateFiles = function(callback) {
-  Manifest.options.files.forEach(function(file) {
-    fs.readFile(file, 'utf8', function(err, data) {
-      if (err) {
-        console.error("With file " + file + ": " + err);
-        process.exit(0);
-      }
+exports.render = function(callback) {
+  var jadeTemplateFile = Generator.options.template;
+  var jadeTemplate = fs.readFileSync(jadeTemplateFile, 'utf8');
+  var fn = jade.compile(jadeTemplate, {filename: jadeTemplateFile, pretty: false});
 
-      Generator.render(Manifest.options, file, path.basename(file, Manifest.options.extension), data);
-    })
+  async.forEach(Manifest.options.files, function(file, cb) {
+    var data = fs.readFileSync(file, 'utf8');//, function(err, data) {
+    Generator.render(Manifest.options, fn, file, path.basename(file, Manifest.options.extension), data, cb);
+  }, function(err, results) {
+    callback(err);
   });
-
-  callback(null);
 };
