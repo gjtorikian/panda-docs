76a6141d2797f6d3cf2ec0eb43b897176f9b1b36
Garen Torikian <gjtorikian@gmail.com>
2012-03-13 12:47:27 +0100

    Add doc testing suite

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 var panda = require("./bin/panda-docs");
 
-panda.make(["./src/manifest.json", "-t", "Panda (from command line)"], function(err) {
-    if (err) console.error(err);
+panda.make(["./src/manifest.json", "-t", "Panda (from script)"], function(err) {
+	if (err !== undefined) {
+		console.error(err);
+	}
 });
